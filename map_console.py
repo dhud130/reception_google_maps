af232e85fc646f4277b4b5aec3319bad38160dc2
dhud130 <147463297+dhud130@users.noreply.github.com>
2023-11-09 13:28:29 -1000

    Add files via upload
    corrected Map_console.py

--- map_console.py
+++ map_console.py
@@ -1,25 +1,34 @@
 from flask import Flask, render_template, request
+from fuzzywuzzy import fuzz
 
 app = Flask(__name__)
 
-
 location = {
     "673 Ayres Ave": ["acs building", "BLDG 750", "soldier support center"],
-    "BLDG 683": ["Desmond Doss Health Clinic", "Desmond Doss", "clinic"],
+    "683 Waianae Ave": ["Desmond Doss Health Clinic", "Desmond Doss", "clinic"],
     "694 McCornack Rd": ["PX", "Post Exchange", "schofield main exchange"],
-    "1470 Foote St": ["tropics", "recreation center", "rec center", "tropics rec center", "bldg 589"]
+    "1470 Foote Ave": ["tropics", "recreation center", "rec center", "tropics rec center", "bldg 589"],
+    "361 Waianae Ave": ["Tropic Lightning Museum", "museum"], 
+    "560 Kolekole Ave": ["IG office", "Inspector General Office", "Inspector General"],
+    "2091 Kolekole Ave": ["25ID Headquarters", "Division HQ", "Division Headquarters"],
+    "698 Trimble Rd": ["Schofield Barracks Commissary", "Commissary"],
+    "1316 Lyman Rd": ["Provost Marshal Office", "Provost Marshal", "Provost"],
+    "540 Humprheys Rd": ["Central Issue Facility", "CIF"],
+    "1020 Menoher Rd": ["Replacement Company", "Reception Company", "Repl-Co"]
+
+
 }
 
-def address_finder(locations, user_input):
-    for key, values in locations.items():
-        if user_input in [val.lower() for val in values]:
-            return key
-    return user_input + " not found in the dictionary"
+@app.route('/')
+def index():
+    return render_template('Homepage.html')
+
+@app.route('/maps')
+def maps():
+    return render_template('maps.html')
 
 if __name__ == '__main__':
-    while True:
-        user_input = input("Enter a location: ").lower()
-        if user_input == 'exit':
-            break
-        result = address_finder(location, user_input)
-        print(f"The location for '{user_input}' is: {result}")
+    app.run(debug=True)
+
+
+
